fd35553f0c7f8f4f2b5ced975749521531804c6d
Adam Julier <adamjulier@ool-ad020067.dyn.optonline.net>
2018-05-29 18:37:01 -0400

    media url regex fixing edit

--- media/urls.py
+++ media/urls.py
@@ -6,9 +6,6 @@ from . import views
 app_name = 'media'
 
 urlpatterns = [
-    url('', views.MediaCreateAPIView.as_view(), name="list"),
-    url('<pk>/$', views.MediaDetailAPIView.as_view(), name="detail"),
-
-    # url(r'^$', views.MediaCreateAPIView.as_view(), name="list"),
-    # url(r'^(?P<pk>[0-9]+)/$', views.MediaDetailAPIView.as_view(), name="detail"),
+    url(r'^$', views.MediaCreateAPIView.as_view(), name="list"),
+    url(r'^(?P<pk>[0-9]+)/$', views.MediaDetailAPIView.as_view(), name="detail"),
 ]
