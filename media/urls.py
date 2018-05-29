a75481d5948c77f82137c4b87a6905d7cdba19a7
Adam Julier <adamjulier@ool-ad020067.dyn.optonline.net>
2018-05-29 18:31:39 -0400

    change urls from regex

--- media/urls.py
+++ media/urls.py
@@ -6,11 +6,9 @@ from . import views
 app_name = 'media'
 
 urlpatterns = [
-    url(r'^$', views.MediaCreateAPIView.as_view(), name="list"),
-    url(r'^(?P<pk>[0-9]+)/$', views.MediaDetailAPIView.as_view(), name="detail"),
+    url('', views.MediaCreateAPIView.as_view(), name="list"),
+    url('<pk>/$', views.MediaDetailAPIView.as_view(), name="detail"),
+
+    # url(r'^$', views.MediaCreateAPIView.as_view(), name="list"),
+    # url(r'^(?P<pk>[0-9]+)/$', views.MediaDetailAPIView.as_view(), name="detail"),
 ]
-
-
-# urlpatterns = [
-#     path('', views.index, name='index')
-# ]
