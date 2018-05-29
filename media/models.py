8161d388b32fecc156ff33c16263d0ccbf8c3ac6
Adam Julier <adamjulier@ool-ad020067.dyn.optonline.net>
2018-05-29 15:48:26 -0400

    media models maybe working

--- media/models.py
+++ media/models.py
@@ -1,3 +1,24 @@
-from django.db import models
+from __future__ import unicode_literals
+from django.conf import settings
 
-# Create your models here.
+from django.db import models
+from django.contrib.postgres.fields import ArrayField
+
+from django.utils.encoding import smart_text as smart_unicode
+from django.utils.translation import ugettext_lazy as _
+
+
+class Media(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    mediatype = models.CharField(_("MediaType"), max_length=255)
+    path = models.CharField(_("Path"), max_length=255)
+    tags = ArrayField(ArrayField(models.CharField(max_length = 5000)))
+    # done = models.BooleanField(_("Done"), default=False)
+    date_created = models.DateTimeField(_("Date Created"), auto_now_add=True)
+
+    class Meta:
+        verbose_name = _("Media")
+        verbose_name_plural = _("Media")
+
+    def __unicode__(self):
+        return smart_unicode(self.name)
