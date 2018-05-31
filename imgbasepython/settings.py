6c1ce5c7f4112a1ad44a08da7625bb7127799f04
Adam Julier <adamjulier@ool-ad020067.dyn.optonline.net>
2018-05-31 17:11:29 -0400

    host

--- imgbasepython/settings.py
+++ imgbasepython/settings.py
@@ -111,7 +111,7 @@ DATABASES = {
         'NAME': 'imgbase',
         'USER': 'imgbaseuser',
         'PASSWORD': '',
-        'HOST': 'localhost',
+        'HOST': '0.0.0.0',
         'PORT': '',
     }
 }
