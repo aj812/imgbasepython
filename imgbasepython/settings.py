b34d730fe510ab519c62683d56642fe0884dcea2
Adam Julier <adamjulier@ool-ad020067.dyn.optonline.net>
2018-05-31 16:36:29 -0400

    rm dj database test

--- imgbasepython/settings.py
+++ imgbasepython/settings.py
@@ -37,9 +37,9 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 # Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+# import dj_database_url
+# db_from_env = dj_database_url.config()
+# DATABASES['default'].update(db_from_env)
 
 
 REST_FRAMEWORK = {
