18a312f9b080455436d77487a95417390a09aa22
Adam Julier <adamjulier@ool-ad020067.dyn.optonline.net>
2018-05-31 15:35:20 -0400

    setting

--- imgbasepython/settings.py
+++ imgbasepython/settings.py
@@ -49,7 +49,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'catalog',
     'rest_framework',
     'rest_framework.authtoken',
     'users',
