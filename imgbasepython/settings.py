64bcdc4ef4b45c2396ffe349c41091d68d7946af
Adam Julier <adamjulier@ool-ad020067.dyn.optonline.net>
2018-05-31 17:05:24 -0400

    switch back

--- imgbasepython/settings.py
+++ imgbasepython/settings.py
@@ -14,9 +14,9 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 
-import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES = { 'default': dj_database_url.config() }
+# import dj_database_url
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES = { 'default': dj_database_url.config() }
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -112,7 +112,7 @@ DATABASES = {
         'NAME': 'imgbase',
         'USER': 'imgbaseuser',
         'PASSWORD': 'password123',
-        'HOST': '0.0.0.0',
+        'HOST': 'localhost',
         'PORT': '',
     }
 }
