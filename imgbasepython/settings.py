92c21c1c3d9f90e30e64a77e5f24f11e0900ae4f
Adam Julier <adamjulier@ool-ad020067.dyn.optonline.net>
2018-05-31 16:43:30 -0400

    djdatabase test

--- imgbasepython/settings.py
+++ imgbasepython/settings.py
@@ -14,9 +14,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 
-# db_from_env = dj_database_url.config()
-#  DATABASES[‘default’].update(db_from_env)
-
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
