74fb8c8000f0fbb9b549cc2f60496faab1fa7bb5
Adam Julier <adamjulier@ool-ad020067.dyn.optonline.net>
2018-05-31 15:33:09 -0400

    static

--- imgbasepython/settings.py
+++ imgbasepython/settings.py
@@ -143,17 +143,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-STATIC_ROOT = "app-root/imgbasepython/wsgi/static"
-
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+    ]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
-STATICFILES_DIRS = (
-    ('assets', 'app-root/imgbasepython/wsgi/openshift/static'),
-
-    )
-
-
-STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
