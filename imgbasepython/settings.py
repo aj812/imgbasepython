46d67fa849368a5a3f74402808d516bffb255f16
Adam Julier <adamjulier@ool-ad020067.dyn.optonline.net>
2018-05-31 15:51:27 -0400

    superuser

--- imgbasepython/settings.py
+++ imgbasepython/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'imgbase',
         'USER': 'imgbaseuser',
-        'PASSWORD': '',
+        'PASSWORD': 'password123',
         'HOST': '0.0.0.0',
         'PORT': '',
     }
