1f663d04910fcb8ae441ab80822c8cd2b6874ea9
Adam Julier <adamjulier@Kevins-Air.home>
2018-06-02 00:34:45 -0400

    overlap query render to view working

--- media/views.py
+++ media/views.py
@@ -12,14 +12,18 @@ from media.serializers import MediaSerializer
 
 
 def SearchImgsView(request):
+    testarr = ['blah', 'fifth']
     images = Media.objects.all()
+    overlapfilter = Media.objects.all().filter(tags__overlap=testarr)
     first = images[0]
     context = {
         "images": images,
+        "overlapfilter": overlapfilter,
+        "terms": testarr,
     }
-    jsontest = json.dumps({ 'id': first.id, 'filename': first.filename, 'mediatype': first.mediatype, 'tags': first.tags, 'uri': first.uri })
+    jsonres = json.dumps({ 'id': first.id, 'filename': first.filename, 'mediatype': first.mediatype, 'tags': first.tags, 'uri': first.uri })
     # return HttpResponse(first.uri)
-    return render(request, 'media/media_list.html', { 'images': images, 'jsontest': jsontest })
+    return render(request, 'media/media_list.html', { 'terms': testarr, 'overlapfilter': overlapfilter, 'images': images, 'jsonres': jsonres })
 
 # def SearchImgsView(request):
 #     html = "<html><body> Hello </body></html>"
